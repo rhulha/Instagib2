04467fc5623925585f6be5684b4d1b30effe549f
Ray <java4life@gmail.com>
2021-01-26 03:46:48 +0100

    moar debug points.

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,8 @@ import { setupScene, setupRenderer } from './setup.js';
 import { Player } from './Player.js';
 import { Brush } from './Brush.js';
 import { addPoints } from './addPoints.js';
+import { trigger_push } from './trigger.js';
+
 
 const clock = new Clock();
 var camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -14,41 +16,35 @@ camera.position.z = 2.13;
 var scene = setupScene();
 const renderer = setupRenderer();
 
-var planes_map = [
-	"( 2488 -256 268 ) ( 2360 -256 188 ) ( 2360 -128 188 ) common/trigger 0 0 0 0.500000 0.500000 134217728 0 0",
-	"( 2360 -128 180 ) ( 2360 -128 164 ) ( 2488 -128 164 ) common/trigger 0 0 0 0.500000 0.500000 134217728 0 0",
-	"( 2480 -128 260 ) ( 2480 -128 220 ) ( 2480 -256 220 ) common/trigger 0 0 0 0.500000 0.500000 134217728 0 0",
-	"( 2360 -256 -4 ) ( 2360 -256 4 ) ( 2488 -256 4 ) common/trigger 0 0 0 0.500000 0.500000 134217728 0 0",
-	"( 2360 -256 180 ) ( 2360 -256 148 ) ( 2360 -128 148 ) common/trigger 0 0 0 0.500000 0.500000 134217728 0 0",
-	"( 2360 -256 164 ) ( 2488 -256 220 ) ( 2488 -128 220 ) common/trigger 0 0 0 0.500000 0.500000 134217728 0 0"
-];
+var brushess = [];
 
-// TODO: Use Three.js Plane, it is pretty good!
-
-var planes = [];
-for(var plane of planes_map) {
-	var p1x, p1y, p1z, p2x, p2y, p2z, p3x, p3y, p3z;
-	[,p1x,p1z,p1y,,,p2x,p2z,p2y,,,p3x,p3z,p3y] = plane.split(" ");
-	var p = new Plane().setFromCoplanarPoints(new Vector3(p1x,p1y,p1z).multiplyScalar(0.038), new Vector3(p2x,p2y,p2z).multiplyScalar(0.038), new Vector3(p3x,p3y,p3z).multiplyScalar(0.038))
-	planes.push(p);
-}
-
-const vertices = [];
-
-const geometry = new Geometry();
-var brush = new Brush(planes);
-var polygons = brush.getPolygons();
-for(var face of polygons) {
-	console.log("new face");
-	for( var point of face) {
-		geometry.vertices.push(	point);
-		console.log(point)
-		vertices.push( point.x, point.y, point.z );
+for( var planes_map of trigger_push) {
+	var planes = [];
+	for(var plane of planes_map) {
+		var p1x, p1y, p1z, p2x, p2y, p2z, p3x, p3y, p3z;
+		[,p1x,p1z,p1y,,,p2x,p2z,p2y,,,p3x,p3z,p3y] = plane.split(" ");
+		var p = new Plane().setFromCoplanarPoints(new Vector3(p1x,p1y,p1z).multiplyScalar(0.038), new Vector3(p2x,p2y,p2z).multiplyScalar(0.038), new Vector3(p3x,p3y,p3z).multiplyScalar(0.038))
+		planes.push(p);
 	}
+	var brush = new Brush(planes);
+
+	const vertices = [];
+	//const geometry = new Geometry();
+	var polygons = brush.getPolygons();
+	for(var face of polygons) {
+		//console.log("new face");
+		for( var point of face) {
+			//geometry.vertices.push(	point);
+			//console.log(point)
+			vertices.push( point.x, point.y, point.z );
+		}
+	}
+	
+	addPoints(scene, vertices);
 }
 
-addPoints(scene, vertices);
 
+/*
 geometry.faces.push(
 	// front
 	new Face3(0, 3, 2),
@@ -74,6 +70,7 @@ var material = new MeshNormalMaterial();
 geometry.computeFaceNormals();
 const mesh = new Mesh(geometry, material);
 scene.add( mesh );
+*/
 
 const worldOctree = new Octree();
 document.body.addEventListener( 'mousemove', ( event ) => {
